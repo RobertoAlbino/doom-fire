6532e47392c23e8d3844dcb6830590e0237dec6f
Roberto Albino Junior <junior_wash@hotmail.com>
2019-01-28 23:24:35 -0200

    Initial

---
+++ run.sh
@@ -0,0 +1,1 @@
+cd bin && sh ./DoomFire && cd ..
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Olá Mundo" << endl;
+    return 0;
+}
